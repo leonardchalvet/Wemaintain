8b8cd3aa9b4c41d1144115777e610777abcfbf23
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-09-25 18:00:28 +0200

    Animation quote last section
    not finished

--- js/index.js
+++ js/index.js
@@ -17,9 +17,72 @@ $(window).on('load', function() {
 	});
 	animScroll();
 
-	$('body').click(function(){
+	/*$('body').click(function(){
 		$('#section-team .wrapper .container-carousel .container-el .el').toggleClass('active');
 		$('#section-quotes .wrapper .container-el .el').toggleClass('active');
-	})
+	})*/
 
+
+	/* QUOTE UNIQUE FUNCTION & INIT */
+
+	//Function
+	function animQuoteSolo(num) {
+		let container = '#section-quotes .wrapper .container-el .el';
+		if(!$(container + ':nth-child('+num+')').hasClass('active')) {
+
+			$(container).removeClass('active');
+			
+			setTimeout(function(){
+				
+				$(container).show();
+				for( let i=1 ; i <= $(container).length ; i++ ) {
+					if(i != num) {
+						$(container + ':nth-child('+i+')').hide();
+					}
+				}
+
+				$(container + ':nth-child('+num+')').addClass('active');
+
+			}, 500)
+		}
+	}
+	
+	//Initialisation
+	for( let i=2 ; i <= $('#section-quotes .wrapper .container-el .el').length ; i++ ) {
+		$('#section-quotes .wrapper .container-el .el:nth-child('+i+')').hide();
+	}
+
+	let numQuoteSolo = 1;
+
+	//Anim auto
+	setInterval(function() {
+		setTimeout(function() { //preparation pause
+			animQuoteSolo(numQuoteSolo);
+			numQuoteSolo = numQuoteSolo >= $('#section-quotes .wrapper .container-el .el').length ? 1 : numQuoteSolo+=1
+		}, 0);
+	}, 5000);
+
+	//Click right arrow
+	$('#section-quotes .wrapper .container-nav .nav:nth-child(1)').click(function(){
+		numQuoteSolo+=1;
+		numQuoteSolo = numQuoteSolo >= $('#section-quotes .wrapper .container-el .el').length ? 1 : numQuoteSolo;
+		console.log(numQuoteSolo);
+	});
+
+	//Click left arrow
+	$('#section-quotes .wrapper .container-nav .nav:nth-child(2)').click(function(){
+		numQuoteSolo-=1;
+		numQuoteSolo = numQuoteSolo <= 0 ? $('#section-quotes .wrapper .container-el .el').length : numQuoteSolo;
+		console.log(numQuoteSolo);
+	});
+
+	/* END QUOTE UNIQUE */
+
+
+
+	/**/
+	function animQuotesMulti(num) {
+
+	}
+	/**/
 })
