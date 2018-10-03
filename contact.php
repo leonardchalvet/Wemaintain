c13d77b5859cd66c90daaad74906528721b5cc28
Léonard Chalvet <leonard@agence-me.com>
2018-10-03 17:54:35 +0200

    Page Contact + Page Techniciens

---
+++ contact.php
@@ -0,0 +1,153 @@
+<html>
+	<head>
+
+		<title>Contact</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/contact.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<header id="header-desktop" class="style-1">
+			<div class="wrapper">
+				<a class="logo" href="">
+					<img class="logo-style-1" src="img/common/logo-style-1.svg" alt="">
+					<img class="logo-style-2" src="img/common/logo-style-2.svg" alt="">
+				</a>
+				<ul class="container-link">
+					<li><a href="">Résidentiel</a></li>
+					<li><a href="">Tertiaire</a></li>
+					<li><a href="">Techniciens</a></li>
+					<li><a href="">A propos</a></li>
+					<li><a href="">FAQ</a></li>
+				</ul>
+				<div class="container-action">
+					<div class="signin">Se connecter</div>
+					<div class="sep"></div>
+					<div class="signup">S’inscrire</div>
+				</div>
+			</div>
+		</header>
+
+		<main>
+
+			<section id="section-cover" class="container__anim">
+				<div class="wrapper">
+					<div class="container-text">
+						<h1 class="anim__slide anim__delayMedium_1">
+							Contact
+						</h1>
+						<p class="anim__slide anim__delayMedium_2">
+							Pour toute demande n’hésitez pas à nous contacter en remplissant le formulaire ci-dessous. 
+						</p>
+					</div>
+					<div class="obj anim__fade anim__delayMedium_3"></div>
+				</div>
+			</section>
+
+			<section id="section-form" class="container__anim">
+				<div class="wrapper anim__slide anim__delayMedium_4">
+					<div class="container-form">
+						<form action="">
+							<div class="container-input">
+								<div class="title">Nom</div>
+								<div class="wrapper-input">
+									<input placeholder="Nom" type="text">
+								</div>
+								<div class="container-error">
+									Vérifiez votre nom
+								</div>
+							</div>
+							<div class="container-input">
+								<div class="title">Prénom</div>
+								<div class="wrapper-input">
+									<input placeholder="Prénom" type="text">
+								</div>
+								<div class="container-error">
+									Vérifiez votre Prénom
+								</div>
+							</div>
+							<div class="container-input error">
+								<div class="title">Mail</div>
+								<div class="wrapper-input">
+									<input type="text" placeholder="Mail">
+								</div>
+								<div class="container-error">
+									Vérifiez votre Mail
+								</div>
+							</div>
+							<div class="container-input">
+								<div class="title">Téléphone</div>
+								<div class="wrapper-input">
+									<input type="text" placeholder="Téléphone">
+								</div>
+								<div class="container-error">
+									Vérifiez votre Téléphone
+								</div>
+							</div>
+							<div class="container-input select">
+								<div class="title">Sujet de votre message </div>
+								<div class="wrapper-input">
+									<select>
+										<option value="" disabled selected>Sujet de votre message </option>
+									  	<option value="valeur1">Valeur 1</option> 
+									  	<option value="valeur2">Valeur 2</option>
+									  	<option value="valeur3">Valeur 3</option>
+									</select>
+								</div>
+							</div>
+							<div class="container-input textarea">
+								<div class="title">Sujet de votre message</div>
+								<div class="wrapper-input">
+									<textarea placeholder="Entrez votre message"></textarea>
+								</div>
+							</div>
+							<div class="container-btn">
+								<!-- ADD CLASS "valid" on ".btn" FOR VALID FORM -->
+								<div class="btn">
+									<div class="btn-text">Envoyer</div>
+									<div class="btn-valid">
+										<img src="img/contact/check.svg" alt="">
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+					<div class="container-map">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10498.221828126949!2d2.3530999!3d48.8666868!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x808ca89b9327a3e4!2sWeMaintain!5e0!3m2!1sfr!2sfr!4v1538557658041" frameborder="0" style="border:0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</section>
+
+
+			<section id="section-experience" class="container__anim">
+				<div class="wrapper">
+					<div class="container-text">
+						<img class="anim__slide anim__delayMedium_1" src="img/home/picto-section-exigez.svg" alt="">
+						<h2 class="anim__slide anim__delayMedium_2">Exigez une nouvelle expérience</h2>
+					</div>
+					<div class="container-btn">
+						<div class="btn anim__slide anim__delayMedium_3">
+							<span class="btn-text">Je demande une pré-visite gratuite</span>
+						</div>
+					</div>
+					<img class="dots anim__fade anim__delayMedium_1" src="img/common/points1.svg" alt="">
+				</div>
+			</section>
+
+		</main>
+
+		<footer></footer>
+
+		<script type="text/javascript" src="js/common.js"></script>
+	</body>
+</html>
---
+++ techniciens.php
@@ -0,0 +1,283 @@
+<html>
+	<head>
+
+		<title>Techniciens</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/techniciens.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<header id="header-desktop">
+			<div class="wrapper">
+				<a class="logo" href="">
+					<img class="logo-style-1" src="img/common/logo-style-1.svg" alt="">
+					<img class="logo-style-2" src="img/common/logo-style-2.svg" alt="">
+				</a>
+				<ul class="container-link">
+					<li><a href="">Résidentiel</a></li>
+					<li><a href="">Tertiaire</a></li>
+					<li><a href="">Techniciens</a></li>
+					<li><a href="">A propos</a></li>
+					<li><a href="">FAQ</a></li>
+				</ul>
+				<div class="container-action">
+					<div class="signin">Se connecter</div>
+					<div class="sep"></div>
+					<div class="signup">S’inscrire</div>
+				</div>
+			</div>
+		</header>
+
+		<main>
+			<section id="section-cover" class="container__anim">
+				<div class="wrapper">
+					<div class="container-text">
+						<h1 class="anim__slide anim__delayMedium_1">	
+							<span>Techniciens</span>, captez la valeur de votre travail
+						</h1>
+						<p class="anim__slide anim__delayMedium_2">
+							Devenez indépendant grâce à la technologie et rejoignez une communauté qui vous ressemble
+						</p>
+						<a class="btn anim__slide anim__delayMedium_3">
+							<span class="btn-text">
+								Commencer
+							</span>
+						</a>
+					</div>
+					<div class="container-illu">
+						<img class="phone anim__slide anim__delayMedium_5" src="img/techniciens/cover/iPhone.png" alt="">
+						<img class="obj-1 anim__slide anim__delayMedium_6" src="img/techniciens/cover/photo1@2x.png" alt="">
+						<img class="obj-2 anim__slide anim__delayMedium_4" src="img/techniciens/cover/photo2.png" alt="">
+						<img class="dots anim__fade anim__delayMedium_7" src="img/common/points1.svg" alt="">
+					</div>
+				</div>
+			</section>
+
+			<section id="section-features" class="container__anim">
+				<div class="wrapper">
+					<div class="container-text">
+						<h2 class="anim__slide anim__delayMedium_1">
+							Rejoignez la communauté WeMaintain
+						</h2>
+						<p class="anim__slide anim__delayMedium_2">
+							Aux Etats-Unis un technicien gagne en moyenne [63 000€/an]. Et si on revalorisait votre métier en France?
+						</p>
+					</div>
+					<div class="container-el">
+						<div class="el anim__slide anim__delayMedium_3">
+							<div class="icn">
+								<img src="img/techniciens/features/picto1-section1.svg" alt="">
+							</div>
+							<h3>
+								Devenez votre propre patron
+							</h3>
+							<p>
+								Nous vous accompagnons dans toutes les démarches
+							</p>
+						</div>
+						<div class="el anim__slide anim__delayMedium_4">
+							<div class="icn">
+								<img src="img/techniciens/features/picto2-section1.svg" alt="">
+							</div>
+							<h3>
+								La technologie à votre service
+							</h3>
+							<p>
+								Une application adaptée à vos besoins vous accompagne pour gérer votre activité au quotidien.
+							</p>
+						</div>
+						<div class="el anim__slide anim__delayMedium_5">
+							<div class="icn">
+								<img src="img/techniciens/features/picto3-section1.svg" alt="">
+							</div>
+							<h3>
+								Vous êtes entouré
+							</h3>
+							<p>
+								Exigence, compétences et valeurs lient la communauté
+							</p>
+						</div>
+					</div>
+					<div class="container-action anim__slide anim__delayMedium_7">
+						<a href="" class="btn">
+							<span class="btn-text">
+								Je veux en savoir plus
+							</span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-signup">
+				<div class="wrapper">
+					<div class="container-text container__anim">
+						<h2 class="anim__slide anim__delayMedium_1">
+							Une solution pensée pour vous
+						</h2>
+						<p class="anim__slide anim__delayMedium_2">
+							Notre but: vous faire gagner du temps et simplifier chaque étape.
+						</p>
+					</div>
+					<div class="container-tsr">
+						<div class="container-illu">
+							
+							<div class="el-illu el-illu-1 container__anim__dt">
+								<img class="img-1 anim__slide anim__delayMedium_1" src="img/techniciens/signup-step-1/Contrat@3x.jpg" alt="">
+								<img class="img-2 anim__slide anim__delayMedium_2" src="img/techniciens/signup-step-1/Sign@3x.jpg" alt="">
+								<img class="obj anim__slide anim__delayMedium_3" src="img/techniciens/signup-step-1/cercle.svg" alt="">
+								<img class="dots anim__fade anim__delayMedium_4" src="img/techniciens/signup-step-1/Oval2.svg" alt="">
+							</div>
+							<div class="el-illu el-illu-2 container__anim__dt">
+								<img class="img anim__slide anim__delayMedium_1" src="img/techniciens/signup-step-2/iPhone.png" alt="">
+								<img class="obj anim__slide anim__delayMedium_2" src="img/techniciens/signup-step-2/circle.svg" alt="">
+								<img class="dots anim__slide anim__delayMedium_3" src="img/techniciens/signup-step-2/Oval.svg" alt="">
+							</div>
+							<div class="el-illu el-illu-3 container__anim__dt">
+								<img class="img-1 anim__slide anim__delayMedium_1" src="img/techniciens/signup-step-3/Browser.png" alt="">
+								<img class="dots anim__slide anim__delayMedium_2" src="img/techniciens/signup-step-3/Oval.svg" alt="">
+								<img class="obj anim__slide anim__delayMedium_3" src="img/techniciens/signup-step-3/technicien.svg" alt="">
+							</div>
+							<div class="el-illu el-illu-4 container__anim__dt">
+								<img class="img-1 anim__slide anim__delayMedium_1" src="img/techniciens/signup-step-4/Screen.png" alt="">
+								<img class="dots anim__slide anim__delayMedium_2" src="img/techniciens/signup-step-4/Oval.svg" alt="">
+								<img class="obj anim__slide anim__delayMedium_3" src="img/techniciens/signup-step-4/technicien.svg" alt="">
+							</div>
+						</div>
+						<div class="container-nav">
+							<div class="container-el">
+								<div class="el" data-illu="1" >Création de votre société</div>
+								<div class="el" data-illu="2">Vos premiers pas</div>
+								<div class="el" data-illu="3">Votre parc</div>
+								<div class="el" data-illu="4">Communauté</div>
+							</div>
+							<div class="container-bar"></div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-ots">
+				<div class="background"></div>
+				<div class="wrapper">
+					<div class="container-head container__anim">
+						<h2 class="anim__slide anim__delayMedium_1">Comment ça marche ?</h2>
+						<p class="anim__slide anim__delayMedium_2">
+							Créez votre première demande de maintenance en seulement trois minutes
+						</p>
+					</div>
+					<div class="container-el">
+						<div class="el container__anim">
+							<div class="container-text">
+								<h3 class="anim__slide anim__delaySmall_1">Rentrez les informations concernant vos bâtiments et ascenseurs.</h3>
+								<p class="anim__slide anim__delaySmall_2">Précisez simplement l'adresse et le nombre d'appareils, pas besoin d'être un expert de l'ascenseur.</p>
+							</div>
+							<div class="container-illu">
+								<img class="img anim__fade anim__delaySmall_3" src="img/home/features-1/Box@2x.png" alt="">
+								<img class="obj-2 anim__slide anim__delaySmall_4" src="img/home/features-1/Lieu@2x.png" alt="">
+								<img class="obj-1 anim__slide anim__delaySmall_5" src="img/home/features-1/Map@2x.png" alt="">
+								<img class="dots anim__slide anim__delaySmall_6" src="img/common/points2.svg" alt="">
+							</div>
+						</div>
+						<div class="el container__anim">
+							<div class="container-text">
+								<h3 class="anim__slide anim__delaySmall_1">Créez une demande de maintenance.</h3>
+								<p class="anim__slide anim__delaySmall_2">Nous nous adaptons à vos conditions et planifions une pré-visite gratuite par l'un des techniciens de la communauté.</p>
+							</div>
+							<div class="container-illu">
+								<img class="img anim__fade anim__delaySmall_3" src="img/home/features-2/Box@2x.png" alt="">
+								<img class="obj-1 anim__slide anim__delaySmall_4" src="img/home/features-2/1@2x.png" alt="">
+								<img class="obj-2 anim__slide anim__delaySmall_5" src="img/home/features-2/Contrat@2x.png" alt="">
+								<img class="dots anim__slide anim__delaySmall_6" src="img/common/points4.svg" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-services" class="container__anim">
+				<div class="wrapper">
+					<div class="container-text">
+						<div class="container-desc">
+							<h2 class="anim__slide anim__delayMedium_1">
+								Un service sur mesure
+							</h2>
+							<p class="anim__slide anim__delayMedium_2">
+								Foncières, Asset / Property / Facility managers, Hôtels, Commerces, nous savons répondre à vos attentes
+							</p>
+						</div>
+					</div>
+					<div class="container-li">
+						<div class="container_illu anim__slide anim__delayMedium_4">
+							<img src="img/tertiaire/services/Illustration.svg" alt="">
+						</div>
+						<div class="container-el">
+							<div class="el anim__slide anim__delayMedium_5">
+								<div class="icn">
+									<img src="img/tertiaire/services/picto1-section3.svg" alt="">
+								</div>
+								<div class="text">
+									<h3>Accompagnement du bureau de contrôle</h3>
+									<p>
+										WeMaintain travaille main dans la main avec les acteurs de l'industrie
+									</p>
+								</div>
+							</div>
+							<div class="el anim__slide anim__delayMedium_6">
+								<div class="icn">
+									<img src="img/tertiaire/services/picto2-section3.svg" alt="">
+								</div>
+								<div class="text">
+									<h3>Temps de dépannage personnalisé</h3>
+									<p>
+										Nous garantissons des temps d'intervention réduits pour vos sites stratégiques
+									</p>
+								</div>
+							</div>
+							<div class="el anim__slide anim__delayMedium_7">
+								<div class="icn">
+									<img src="img/tertiaire/services/picto3-section3.svg" alt="">
+								</div>
+								<div class="text">
+									<h3>Reporting</h3>
+									<p>
+										Une vue claire sur votre patrimoine en gestion
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-experience" class="container__anim">
+				<div class="wrapper">
+					<div class="container-text">
+						<img class="anim__slide anim__delayMedium_1" src="img/home/picto-section-exigez.svg" alt="">
+						<h2 class="anim__slide anim__delayMedium_2">Exigez une nouvelle expérience</h2>
+					</div>
+					<div class="container-btn">
+						<div class="btn anim__slide anim__delayMedium_3">
+							<span class="btn-text">Je demande une pré-visite gratuite</span>
+						</div>
+					</div>
+					<img class="dots anim__fade anim__delayMedium_1" src="img/common/points1.svg" alt="">
+				</div>
+			</section>
+		</main>
+
+		<footer></footer>
+
+		<script type="text/javascript" src="js/common.js"></script>
+		<script type="text/javascript" src="js/techniciens.js"></script>
+	</body>
+</html>
